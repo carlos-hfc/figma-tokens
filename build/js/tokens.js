f44defce7162d900c238183bff64370e6b608dc1
carlos-hfc <carlos-hfc@users.noreply.github.com>
2024-04-05 17:14:07 +0000

    style: Update design tokens

--- build/js/tokens.js
+++ build/js/tokens.js
@@ -1,6 +1,6 @@
 /**
  * Do not edit directly
- * Generated on Fri, 05 Apr 2024 16:45:52 GMT
+ * Generated on Fri, 05 Apr 2024 17:14:07 GMT
  */
 
 module.exports = {
@@ -9,5 +9,6 @@ module.exports = {
   "ColorGray500": "#50505c",
   "ColorGray800": "#1c1c1f",
   "ColorGray900": "#121214",
+  "ColorPurple400": "#9b74f3",
   "ColorPurple500": "#8257e6"
 };
